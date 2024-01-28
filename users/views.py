7e0b59ff9e10556563595b501eedb4f7173760f2
--global <--global>
2024-01-28 22:41:33 +0300

    all cart templates were included

--- users/views.py
+++ users/views.py
@@ -68,8 +68,14 @@ def profile(request):
     }
     return render(request, 'users/profile.html', context)
 
+
+def users_cart(request):
+    return render(request, 'users/users_cart.html')
+
+
 @login_required
 def logout(request):
     messages.success(request, 'Вы вышли из аккаунта')
     auth.logout(request)
     return redirect(reverse('main:index'))
+    
