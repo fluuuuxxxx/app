4df6a6cecca046fdf2c8074e6480a5f96bf933fb
--global <--global>
2024-01-31 01:44:05 +0300

    change cart and all ajax done, but with one problem

--- carts/views.py
+++ carts/views.py
@@ -25,7 +25,6 @@ def cart_add(request):
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
     user_cart = get_user_carts(request)
-
     cart_items_html = render_to_string(
         "carts/includes/included_cart.html", {"carts": user_cart}, request=request)
     
@@ -37,8 +36,25 @@ def cart_add(request):
     return JsonResponse(response_data)
 
 
-def cart_change(request, product_slug):
-    ...
+def cart_change(request):
+    cart_id = request.POST.get("cart_id")
+    quantity = request.POST.get("quantity")
+
+    cart = Cart.objects.get(id=cart_id)
+
+    cart.quantity = quantity
+    cart.save()
+    
+    cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        "carts/includes/included_cart.html", {"carts": cart}, request=request)
+    
+    response_data = {
+        "message": "Количетсво изменено",
+        "cart_items_html": cart_items_html,
+    }
+
+    return JsonResponse(response_data)
 
 def cart_remove(request):
 
